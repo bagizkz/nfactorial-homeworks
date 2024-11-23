b728ab113a0b528c23cb7349feb6175ed54a2cb7
Baglan Orazbayev <or.baglane@gmail.com>
2024-11-24 03:43:00 +0500

    Добавлены тесты и настройка pytest с poetry

---
+++ assignment14/main.py
@@ -0,0 +1,3 @@
+# функция плюс 2 числел
+def calculate(a, b):
+    return a + b
---
+++ assignment14/test_main.py
@@ -0,0 +1,8 @@
+# тестируем main.py
+from main import calculate
+
+def test_calculate():
+    assert calculate(2, 3) == 5, "Ошибка: calculate(2, 3) должно быть 5"
+    assert calculate(-1, 1) == 0, "Ошибка: calculate(-1, 1) должно быть 0"
+    assert calculate(0, 0) == 0, "Ошибка: calculate(0, 0) должно быть 0"
+    assert calculate(100, 200) == 300, "Ошибка: calculate(100, 200) должно быть 300"
